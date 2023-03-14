5a571668e672e70172440ac0c31b8a98ac1d668b
Mairi Wood <MairiMW@Eraon-Mac.local>
2023-03-14 11:20:42 +0000

    see individual review

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,11 +2,18 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://mairi-games-api.onrender.com/api'
-})
+});
 
 export const getReviews = () => {
     return api.get(`/reviews`)
     .then(({data}) => {
         return data.reviews
+    });
+};
+
+export const getReview = (reviewID) => {
+    return api.get(`/reviews/${reviewID}`)
+    .then(({data}) => {
+        return data.review
     });
 };
